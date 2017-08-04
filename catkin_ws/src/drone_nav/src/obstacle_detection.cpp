0b9345e5cd5d31b8866b15190f4f86388cab6361
gitonas <arke@envian.de>
2017-08-04 18:27:40 +0200

    Empty files for structure added

---
+++ catkin_ws/src/drone_nav/src/obstacle_detection.cpp
@@ -0,0 +1,11 @@
+#include "stdio.h"
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "nav_msgs/Odometry.h"
+// #include "pcl_ros/point_cloud.h"
+// #include "pcl/point_types.h"
+
+void poseCallback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+  Boolean a = tru
+}
